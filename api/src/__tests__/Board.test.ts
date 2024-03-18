6dca23326728bc7fe90fbd909188ad67ad3e4cfd
areeven <emilie.ost1990@gmail.com>
2024-03-18 11:24:30 +0100

    Removed unused code

--- api/src/__tests__/Board.test.ts
+++ api/src/__tests__/Board.test.ts
@@ -11,10 +11,6 @@ describe("Test grid pattern and positioning", () => {
     expect(board.getHeight()).toEqual(100);
   });
 
-  test("Top left corner is 0,0", () => {
-    const board = new Board(100, 100, { x: 2, y: 2 });
-  });
-
   test("Grid pattern is 45x45", () => {
     const board = new Board(45, 45, { x: 2, y: 2 });
     expect(board.getWidth()).toEqual(45);
